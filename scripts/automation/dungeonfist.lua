dd081b1105f2b3f5f1c507116d88b81b8930234d
nworbetan <nworbetan@gmail.com>
2012-12-15 21:02:17 -0600

    no, add *this* dungeon fist automation

--- scripts/automation/dungeonfist.lua
+++ scripts/automation/dungeonfist.lua
@@ -1,153 +1,3 @@
--- post_page("/arcade.php", { action = "game", whichgame = "3", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "3", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "2", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "3", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "3", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- post_page("/choice.php", { whichchoice = "486", option = "1", pwd = get_pwd() })
--- output = post_page("/choice.php", { whichchoice = "486", option = "3", pwd = get_pwd() })
-
---[[
-post_page("/arcade.php", { action = "game", whichgame = "3", pwd = get_pwd() })
-
-opt_list = {3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1}
-
-for o in pairs(opt_list) do post_page("/choice.php", { whichchoice = "486", option = opt_list[o], pwd = get_pwd() }) end
-output = post_page("/choice.php", { whichchoice = "486", option = "3", pwd = get_pwd() })
-]]--
-
-function show_link(match, link)
-	if setting_enabled("run automation scripts") then
-		local function newtext(x)
-			return [[
-<script language="javascript">
-function automate_N_times(url) {
-	N = prompt('Play how many games of Dungeon Fist?\nYou have ]] .. count_inventory("Game Grid token") .. [[ Game Grid tokens');
-	if (N > 0) {
-		top.mainpane.location.href = (url + "&numtimes=" + N);
-	}
-}
-</script><br><a href="javascript:automate_N_times(']] .. link(x) .. [[')" style="color:green">{ Automate Dungeon Fist }</a>]]
-		end
-		text = text:gsub("(" .. match .. ")", function(alltext, a, b, c) return alltext .. " " .. newtext(a, b, c) .. "\n" end)
-	end
-end
-
 local automate_dungeon_fist_href = add_automation_script("automate-dungeonfist", function ()
 	local numtimes = tonumber(params.numtimes)
 	if numtimes then
@@ -158,7 +8,7 @@ local automate_dungeon_fist_href = add_automation_script("automate-dungeonfist",
 			if pt:match("bgcolor=blue><b>Dungeon Fist!</b>") then
 				print("Dungeon Fisting " .. i .. " / " .. numtimes)
 			else
-				return pt, pturl
+				break
 			end
 
 			local opts = {3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1}
@@ -172,7 +22,7 @@ local automate_dungeon_fist_href = add_automation_script("automate-dungeonfist",
 			if pt:match("class=effect>You acquire <b>30 Game Grid tickets</b>") then
 				print("A Winner is You!")
 			else
-				return pt, pturl
+				break
 			end
 		end
 		return pt, pturl
@@ -180,7 +30,18 @@ local automate_dungeon_fist_href = add_automation_script("automate-dungeonfist",
 end)
 
 add_printer("/arcade.php", function()
-	show_link([[<a href=town_wrong.php>Back [^<]+</a>]], function ()
-		return automate_dungeon_fist_href { pwd = session.pwd }
-	end)
+	if setting_enabled("run automation scripts") then
+		local function newtext(x)
+			return [[
+<script language="javascript">
+function automate_N_times(url) {
+	N = prompt('Play how many games of Dungeon Fist?\nYou have ]] .. count_inventory("Game Grid token") .. [[ Game Grid tokens');
+	if (N > 0) {
+		top.mainpane.location.href = (url + "&numtimes=" + N);
+	}
+}
+</script><br><a href="javascript:automate_N_times('/kolproxy-automation-script?pwd=]] .. session.pwd .. [[&automation-script=automate-dungeonfist')" style="color:green">{ Automate Dungeon Fist }</a>]]
+		end
+		text = text:gsub("(<a href=town_wrong.php>Back [^<]+</a>)", function(alltext, a, b, c) return alltext .. " " .. newtext(a, b, c) .. "\n" end)
+	end
 end)
