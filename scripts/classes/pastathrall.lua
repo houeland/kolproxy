d6f5227ee3490f524083fea1d200659c95e88d17
bleary <tewhalen+github@gmail.com>
2014-03-28 19:18:35 -0500

    add compact effect mode, add pastathrall support, fix avatar blanking issue

--- scripts/classes/pastathrall.lua
+++ scripts/classes/pastathrall.lua
@@ -1,12 +1,12 @@
 local thralls = {
-	[1] = { name = "Vampieroghi", effect = "+HP" },
-	[2] = { name = "Vermincelli", effect = "+MP" },
-	[3] = { name = "Angel Hair Wisp", effect = "+init%" },
-	[4] = { name = "Elbow Macaroni", effect = "mus=mys" },
-	[5] = { name = "Penne Dreadful", effect = "mox=mys" },
-	[6] = { name = "Lasagmbie", effect = "+meat%" },
-	[7] = { name = "Spice Ghost", effect = "+item%" },
-	[8] = { name = "Spaghetti Elemental", effect = "+stat" },
+   [1] = { name = "Vampieroghi", effect = "+HP" , desc = {"attack/heal", "dispels negative effects", "+60 HP"}, img = "t_vampieroghi"},
+   [2] = { name = "Vermincelli", effect = "+MP", desc = {"restores MP", "attack + poison", "+30 MP" }, img = "t_vermincelli"},
+   [3] = { name = "Angel Hair Wisp", effect = "+init%", desc = {"+init", "prevents enemy crits", "blocks"}, img = "t_wisp"  },
+   [4] = { name = "Elbow Macaroni", effect = "mus=mys", desc = {"mus >= myst", "+weapon damage", "+10% crit chance"}, img="t_elbowmac"},
+   [5] = { name = "Penne Dreadful", effect = "mox=mys", desc = {"mox >= myst", "delevel", "DR +10"}, img = "t_dreadful" },
+   [6] = { name = "Lasagmbie", effect = "+meat%", desc = {"+meat", "spooky attack", "+10 spooky spell dam"}, img="t_lasagmbie" },
+   [7] = { name = "Spice Ghost", effect = "+item%", desc = {"+item", "spices", "better entangling"}, img="t_spiceghost" },
+   [8] = { name = "Spaghetti Elemental", effect = "+stat", desc = {"+stat", "prevents 1st attack", "+5 spell dam"}, img = "t_spagdemon" },
 }
 
 function maybe_get_pastathrall_name(thrallid)
@@ -16,6 +16,14 @@ end
 function maybe_get_pastathrall_effect(thrallid)
 	return (thralls[thrallid] or {}).effect
 end
+
+function maybe_get_pastathrall_desc(thrallid)
+	return (thralls[thrallid] or {}).desc
+end
+function maybe_get_pastathrall_img(thrallid)
+	return (thralls[thrallid] or {}).img
+end
+
 
 function describe_pastathrall(thrallid)
 	if thralls[thrallid] then
