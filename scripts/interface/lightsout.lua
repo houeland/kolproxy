8cb83c2fbc6468c66fa9d8cf850d60c202cfafcd
Tor Gunnar Houeland <houeland@pvv.org>
2014-05-21 01:45:37 +0200

    Lights Out code formatting fixes

--- scripts/interface/lightsout.lua
+++ scripts/interface/lightsout.lua
@@ -1,93 +1,68 @@
-function lightsout() return (-tonumber(status().turnsplayed)) % 37 end 
-
-
-liz_quest = {
+local liz_quest = {
 	{
-		marker = "You carefully tiptoe through the room to look out the window.", 
-		counter = 1, 
-		description =  "store room (window)"
-	},	
+		marker = "You carefully tiptoe through the room to look out the window.",
+		description = "Store room (window)",
+	},
 	{
-		marker = "You approach an overflowing hamper of white sheets and towels with curious red-brown stains on them.", 
-		counter = 2, 
-		description =  "laundry room (stain)"
-	},	
+		marker = "You approach an overflowing hamper of white sheets and towels with curious red-brown stains on them.",
+		description = "Laundry room (stain)",
+	},
 	{
-		marker = "You peer into the bathtub.",  
-		counter = 3, 
-		description =  "bath room (tub)"
-	},	
+		marker = "You peer into the bathtub.",
+		description = "Bath room (tub)",
+	},
 	{
-		marker = "You fumble your way to the pantry and open the door.", 
-		counter = 4, 
-		description = "kitchen (snack)"  
-	},	
+		marker = "You fumble your way to the pantry and open the door.",
+		description = "Kitchen (snack)",
+	},
 	{
-		marker = "You try to remember the signs you saw before the lights went out, and feel your way to the children's section.", 
-		counter = 5, 
-		description =  "library (children's book)"
-	},	
+		marker = "You try to remember the signs you saw before the lights went out, and feel your way to the children's section.",
+		description = "Library (children's book)",
+	},
 	{
-		marker = "You close your eyes, hold up your hands as if you had an invisible partner, and begin to box-step your way across the dance floor.", 
-		counter = 6, 
-		description =  "ball room (dance with yourself)"
-	},	
+		marker = "You close your eyes, hold up your hands as if you had an invisible partner, and begin to box-step your way across the dance floor.",
+		description = "Ball room (dance with yourself)",
+	},
 	{
-		marker = "xxxxx????xxxx", 
-		counter = 7, 
-		description =  "gallery (tormented souls), FIGHT"
+		marker = nil,
+		description = "Gallery (tormented souls), FIGHT",
 	}
 }
 
-
-
-steve_quest = {
+local steve_quest = {
 	{
-		marker = "You reach out your left hand and fumble about.", 
-		counter = 1, 
-		description =  "Bedroom (left nightstand)"
-	},	
+		marker = "You reach out your left hand and fumble about.",
+		description = "Bedroom (left nightstand)",
+	},
 	{
-		marker = "Surprisingly, the contents of the box are pretty mundane", 
-		counter = 2, 
-		description =  "Nursery (stuffed animals)"
-	},	
+		marker = "Surprisingly, the contents of the box are pretty mundane",
+		description = "Nursery (stuffed animals)",
+	},
 	{
-		marker = "You brush away the moss to reveal an engraved portrait of Crumbles", 
-		counter = 3, 
-		description = "Conservatory (graves, Crumbles)"  
-	},	
-	
+		marker = "You brush away the moss to reveal an engraved portrait of Crumbles",
+		description = "Conservatory (graves, crumbles)",
+	},
 	{
-		marker = "Why are they here and not in the wine cellar?", 
-		counter = 4, 
-		description =  "Billiards (taxidermy)"
-	},	
-	
+		marker = "Why are they here and not in the wine cellar?",
+		description = "Billiards (taxidermy)",
+	},
 	{
-		marker = "It is, of course, Stephen's handwriting, though identifiably more mature than in the child's diary you found earlier.", 
-		counter = 5, 
-		description =  "Wine (pinot noir)"
-	},	
-	
+		marker = "It is, of course, Stephen's handwriting, though identifiably more mature than in the child's diary you found earlier.",
+		description = "Wine (pinot noir)",
+	},
 	{
-		marker = "It turns out to be a blackened and slightly melted chain dog-collar.", 
-		counter = 6, 
-		description =  "Boiler (barrel)"
-	},	
-	
+		marker = "It turns out to be a blackened and slightly melted chain dog-collar.",
+		description = "Boiler (barrel)",
+	},
 	{
-		marker = "xxxx????xxxx", 
-		counter = 7, 
-		description =  "Laboratory (weird machine), FIGHT"
+		marker = nil,
+		description = "Laboratory (weird machine), FIGHT"
 	}
 }
 
-
-
-function check_lightsout_questlog()
+local function check_lightsout_questlog()
 	---if !session["checked lights out completion"]
-	ql = get_page("/quest.php", {which=2})
+	local ql = get_page("/quest.php", { which = 2 })
 	if ql:contains("Like Father Like Daughter") then
 		ascension["liz quest"] = 7
 	end
@@ -96,28 +71,27 @@ function check_lightsout_questlog()
 		ascension["steve quest"] = 7
 	end
 
-	print("Checked questlog for lightsout info")
+	print("DEBUG: Checked questlog for lightsout info")
 
 end
 
-
-function check_lightsout_progress()
+local function check_lightsout_progress()
 	local liz = 0
 	local steve = 0
 
-	for _, obj in  ipairs(liz_quest) do
-		if text:contains(obj.marker) then
-			liz = obj.counter
+	for ctr, obj in ipairs(liz_quest) do
+		if obj.marker and text:contains(obj.marker) then
+			liz = ctr
 		end
 	end
 	if liz > tonumber(ascension["liz quest"] or 0) then
-		print("Updating liz quest to " .. liz)
-		ascension["liz quest"] = liz 
+		print("DEBUG: Updating liz quest to " .. liz)
+		ascension["liz quest"] = liz
 	end
 
-	for _, obj in  ipairs(steve_quest) do
-		if text:contains(obj.marker) then
-			steve = obj.counter
+	for ctr, obj in ipairs(steve_quest) do
+		if obj.marker and text:contains(obj.marker) then
+			steve = ctr
 		end
 	end
 	if steve > tonumber(ascension["steve quest"] or 0) then
@@ -132,7 +106,11 @@ function get_lightsout_info()
 	local lastencounter = (ascension["last lights out"] or {}).encounter
 	local liznumber = ascension["liz quest"] or 0
 	local stevenumber = ascension["steve quest"] or 0
-	local counter = lightsout()
+	local function lightsout_turns_remaining()
+		local played = tonumber(status().turnsplayed) or 0
+		return (37 - played % 37) % 37
+	end
+	local counter = lightsout_turns_remaining()
 	if lastturn == turn and counter == 0 then
 		counter = 37
 	end
@@ -141,11 +119,10 @@ end
 
 add_processor("/choice.php", function()
 	if (adventure_title and adventure_title:contains("Lights Out in the")) or (adventure_result and adventure_result:contains("Lights Out in the")) then
-		print("Lights Out!")
-		ascension["last lights out"] = {encounter = adventure_title, turn = turnsthisrun()}
+		print("DEBUG: Lights Out!")
+		ascension["last lights out"] = { encounter = adventure_title, turn = turnsthisrun() }
 		check_lightsout_progress()
 	end
-
 end)
 
 add_processor("/fight.php", function()
@@ -157,36 +134,26 @@ add_processor("/fight.php", function()
 	end
 end)
 
-
 add_charpane_line(function()
-	normalname  = "Lights Out"
-	compactname = "Lights Out"
-	value, lastencounter, liznumber, stevenumber = get_lightsout_info()
-	
-	tooltip = "L/S progress: " .. liznumber .. "/" .. stevenumber
-	if value == 0 then
-		color = "green"
-	else
-		color = "black"
-	end
-	lines = {}
-	table.insert(lines, { normalname = normalname, compactname = compactname, value = value, tooltip = tooltip, color = color })
-	if value==0 and lastencounter then
+	local value, lastencounter, liznumber, stevenumber = get_lightsout_info()
+
+	local tooltip = "L/S progress: " .. liznumber .. "/" .. stevenumber
+	local color = (value == 0) and "green" or "black"
+	local lines = {}
+	table.insert(lines, { name = "Lights Out", value = value, tooltip = tooltip, color = color })
+	if value == 0 and lastencounter then
 		if stevenumber < 7 then
-			table.insert(lines, { name = "Steve", value = steve_quest[stevenumber+1].description })
+			table.insert(lines, { name = "Steve", value = steve_quest[stevenumber + 1].description })
 		end
 		if liznumber < 7 then
-			table.insert(lines, { name = "Liz", value = liz_quest[liznumber+1].description })
+			table.insert(lines, { name = "Liz", value = liz_quest[liznumber + 1].description })
 		end
-		
 	end
 	return lines
-end )
-
-
+end)
 
 add_always_adventure_warning(function()
-	counter, lastencounter, liznumber, stevenumber = get_lightsout_info()
+	local counter, lastencounter, liznumber, stevenumber = get_lightsout_info()
 	if counter == 0 and (not session["checked lights out quest"]) then
 		check_lightsout_questlog()
 		session["checked lights out quest"] = true
@@ -198,22 +165,22 @@ add_always_adventure_warning(function()
 			msg = msg .. "\n<br>(Last one was " .. lastencounter .. ".)"
 		end
 
-		liz_hints = "\n<br/> &nbsp; &nbsp; <b>Elizabeth: </b>"
-		for _, obj in ipairs(liz_quest) do
-			if obj.counter <= liznumber then
+		local liz_hints = "\n<br/> &nbsp; &nbsp; <b>Elizabeth: </b>"
+		for ctr, obj in ipairs(liz_quest) do
+			if ctr <= liznumber then
 				liz_hints = liz_hints .. "<i>" .. obj.description .. ";</i> "
-			elseif obj.counter == liznumber + 1 then
+			elseif ctr == liznumber + 1 then
 				liz_hints = liz_hints .. "<span style='color: green'>" .. obj.description .. ";</span> "
 			else
 				liz_hints = liz_hints .. obj.description .. "; "
 			end
 		end
 
-		steve_hints = "\n<br/> &nbsp; &nbsp; <b>Steven: </b>"
-		for _, obj in ipairs(steve_quest) do
-			if obj.counter <= stevenumber then
+		local steve_hints = "\n<br/> &nbsp; &nbsp; <b>Steven: </b>"
+		for ctr, obj in ipairs(steve_quest) do
+			if ctr <= stevenumber then
 				steve_hints = steve_hints .. "<i>" .. obj.description .. ";</i> "
-			elseif obj.counter == stevenumber + 1 then
+			elseif ctr == stevenumber + 1 then
 				steve_hints = steve_hints .. "<span style='color: green'>" .. obj.description .. ";</span> "
 			else
 				steve_hints = steve_hints .. obj.description .. "; "
