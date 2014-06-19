bd051a8568a786b4ef1f2715949f49dce0a5fdd1
Tim Martin <starwed@gmail.com>
2014-06-19 05:31:17 -0400

    Tweak hidden city to use matches, buy scorpion bowls

--- scripts/automation/scripts.lua
+++ scripts/automation/scripts.lua
@@ -2596,6 +2596,10 @@ mark m_done
 					noncombats = { ["Legend of the Temple in the Hidden City"] = "Open the door" },
 				}
 			}
+		elseif have_item("book of matches") and not citypt:contains("Hidden Tavern") then
+			use_item("book of matches")
+			citypt = get_page("/place.php", { whichplace = "hiddencity" })
+			did_action = citypt:contains("Hidden Tavern")
 		elseif citypt:contains("Hidden Apartment Building") and citypt:contains("Hidden Hospital") and citypt:contains("Hidden Office Building") and citypt:contains("Hidden Bowling Alley") then
 			local spherecount = count_item("scorched stone sphere") + count_item("moss-covered stone sphere") + count_item("crackling stone sphere") + count_item("dripping stone sphere")
 
@@ -2653,6 +2657,9 @@ mark m_done
 				}
 			elseif not have_item("scorched stone sphere") then
 				-- sniff pygmy bowler?
+				if citypt:contains("Hidden Tavern") and not have_item("Bowl of Scorpions") and meat() > 2000 then
+					shop_buy_item("Bowl of Scorpions", "hiddentavern")
+				end
 				script.bonus_target { "item" }
 				return run_task {
 					message = "do bowling alley",
@@ -2694,9 +2701,16 @@ mark m_done
 				action = adventure {
 					zone = "The Hidden Park",
 					macro_function = macro_noodleserpent,
-					noncombats = {
-						["Where Does The Lone Ranger Take His Garbagester?"] = "Knock over the dumpster",
-					}
+					choice_function = function(advtitle, choicenum, pagetext)
+						if advtitle == "Where Does The Lone Ranger Take His Garbagester?" then
+							if ascension["knocked over dumpster"] then
+								return "Dig through the dumpster"
+							else 
+								ascension["knocked over dumpster"] = true
+								return "Knock over the dumpster"
+							end
+						end
+					end,
 				}
 			}
 		elseif not can_wear_weapons() then
