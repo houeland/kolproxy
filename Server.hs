5060d221a366fbf107831939ddc89d42da8c703f
Tor Gunnar Houeland <houeland@pvv.org>
2015-07-26 05:22:01 +0200

    Refactored ref setup from Server.hs to kolproxy.hs

--- Server.hs
+++ Server.hs
@@ -26,8 +26,6 @@ import Network.HTTP
 import Network.Stream (ConnError (ErrorClosed))
 import Network.URI
 import qualified System.Directory (doesFileExist)
-import qualified System.IO
-import qualified System.Random
 import Text.Regex.TDFA
 import qualified Data.ByteString.Char8
 import qualified Data.Map
@@ -141,86 +139,6 @@ handle_kol_request sessionmaster mvsequence mvchat logchan dropping_logchan glob
 
 	writeChan (if isChat then mvchat else mvsequence) (uri, params, baseref, send_response)
 
-make_globalref = do
-	environment_settings <- do
-		let checkenv defaultvalue setting = do
-			value <- getEnvironmentSetting setting
-			case value of
-				Just "1" -> return True
-				Just "0" -> return False
-				_ -> return defaultvalue
-		listenpublic <- checkenv False "KOLPROXY_LISTEN_PUBLIC"
-		actionbarstate <- checkenv True "KOLPROXY_STORE_STATE_IN_ACTIONBAR"
-		localstate <- checkenv (not listenpublic) "KOLPROXY_STORE_STATE_LOCALLY"
-		launchbrowser <- checkenv (not listenpublic) "KOLPROXY_LAUNCH_BROWSER"
-		return $ EnvironmentSettings {
-			store_state_in_actionbar_ = actionbarstate,
-			store_state_locally_ = localstate,
-			store_ascension_logs_ = not listenpublic,
-			store_chat_logs_ = not listenpublic,
-			store_info_logs_ = not listenpublic,
-			listen_public_ = listenpublic,
-			launch_browser_ = launchbrowser
-		}
-
-	let openlog filename = do
-		h <- System.IO.openFile ("logs/info/" ++ filename) System.IO.AppendMode
-		System.IO.hSetBuffering h System.IO.LineBuffering
-		return h
-
-	indentref <- newIORef 0
-	blockluaref <- newIORef False
-	hfiles <- openlog "files-downloaded.txt"
-	htiming <- openlog "timing-log.txt"
-	hhttp <- openlog "http-log.txt"
-	shutdown_secret <- get_md5 <$> show <$> (System.Random.randomIO :: IO Integer)
-
-	chatopendb <- create_db "sqlite3 chatlog" "chat-log.sqlite3"
-	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS public(mid INTEGER PRIMARY KEY NOT NULL, time INTEGER NOT NULL, channel TEXT NOT NULL, playerid INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
-	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS private(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time INTEGER NOT NULL, playerid INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
-	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS other(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
-	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS unrecognized(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, rawjson TEXT NOT NULL);" []
-	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS oldchat(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text TEXT NOT NULL);" []
-
-	chatlogchan <- newChan
-	forkIO_ "kps:chatlogchan" $ forever $ do
-		chanaction <- readChan chatlogchan
-		chanaction chatopendb
-
-	use_slow_http_ref <- newIORef =<< do
-		envhttp10 <- getEnvironmentSetting "KOLPROXY_USE_HTTP10"
-		case envhttp10 of
-			Just "1" -> return True
-			Just "0" -> return False
-			_ -> KoL.HttpLowlevel.check_for_http10
-	tnow <- getCurrentTime
-	last_datafile_update_ref <- newIORef $ addUTCTime (fromInteger (-60000)) tnow
-
-	return GlobalRefStuff {
-		logindents_ = indentref,
-		blocking_lua_scripting_ = blockluaref,
-		h_files_downloaded_ = hfiles,
-		h_timing_log_ = htiming,
-		h_http_log_ = hhttp,
-		shutdown_secret_ = shutdown_secret,
-		doChatLogAction_ = \action -> writeChan chatlogchan action,
-		use_slow_http_ref_ = use_slow_http_ref,
-		lastDatafileUpdate_ = last_datafile_update_ref,
-		environment_settings_ = environment_settings
-	}
-
-kolproxy_setup_refstuff = do
-	(logchan, dropping_logchan) <- do
-		dropping_logchan <- newChan
-		forkIO_ "kps:droplogchan" $ forever $ readChan dropping_logchan
-		logchan <- newChan
-		forkIO_ "kps:logchan" $ forever $ debug_do "writelog" $ join $ readChan $ logchan
-		return (logchan, dropping_logchan)
-
-	globalref <- make_globalref
-
-	return (logchan, dropping_logchan, globalref)
-
 handleConnection globalref sessionmaster sock (portnum, mvsequence, mvchat, logchan, dropping_logchan) = do
 	(sh, _) <- debug_do "accept socket" $ Network.Socket.accept sock
 	h <- debug_do "socket connection" $ socketConnection "???" (fromIntegral portnum) sh
@@ -277,70 +195,6 @@ handleConnection globalref sessionmaster sock (portnum, mvsequence, mvchat, logc
 				_ -> do
 					handle_kol_request sessionmaster mvsequence mvchat logchan dropping_logchan globalref req send_response
 					return True
-
-runProxyServer r rchat portnum = do
-	(logchan, dropping_logchan, globalref) <- kolproxy_setup_refstuff
-
-	-- TODO: get rid of fakeref here!
-	let _fake_other = OtherRefStuff {
-		connection_ = undefined,
-		sessionData_ = undefined
-	}
-	let logref = LogRefStuff { logchan_ = logchan, solid_logchan_ = logchan }
-	let _log_fakeref = RefType { logstuff_ = logref, processPage_ = undefined, otherstuff_ = _fake_other, stateValid_ = undefined, globalstuff_ = globalref }
-
-	let try_handling send_response x = do
-		result <- try x
-		(send_response $ case result of
-			Right resp -> resp
-			Left err -> mkResponse (5,0,0) [("Content-Type", "text/plain; charset=UTF-8"), ("Cache-Control", "no-cache")] $ Data.ByteString.Char8.pack $ "Error:\n\n" ++ show (err :: SomeException)) `catch` (\e -> do
-				putWarningStrLn $ "send_response exception: " ++ show (e :: SomeException))
-
-	mvsequence <- newChan
-	forkIO_ "kps:mvseq" $ forever $ do
-		(uri, params, cookie, send_response) <- readChan mvsequence
-		try_handling send_response $ debug_do ("sequence response for: " ++ show uri) $ do
-			Logging.log_file_retrieval _log_fakeref ("proxy:" ++ show uri) params
-			Logging.log_time_uri _log_fakeref "request" uri
-			resp <- Logging.log_time_interval _log_fakeref ("creating response for: " ++ show uri) $ r uri params cookie
-			return resp
-	mvchat <- newChan
-	forkIO_ "kps:mvchat" $ forever $ do
-		(uri, params, cookie, send_response) <- readChan mvchat
-		forkIO_ "kps:mvchattry" $ do -- Serve asynced, possible private-message loss until server is fixed
--- 		do -- Serve synced, possible hang on timeouts, still can't guarantee private-messages
-			try_handling send_response $ debug_do ("chat response for: " ++ show uri) $ rchat uri params cookie
-
-	sessionmastermv <- newMVar Data.Map.empty
-
-	dblogmapmv <- newMVar Data.Map.empty
-	let dblogstuff filename action = do
-		chan <- modifyMVar dblogmapmv $ \m -> do
-			case Data.Map.lookup filename m of
-				Just x -> return (m, x)
-				Nothing -> do
-					putStrLn $ "DB: Opening log database: " ++ filename
-					opendb <- create_db "sqlite3 log" filename
-					do_db_query_ opendb "CREATE TABLE IF NOT EXISTS pageloads(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time TEXT NOT NULL, statusbefore TEXT, statusafter TEXT, statebefore TEXT, stateafter TEXT, sessionid TEXT NOT NULL, requestedurl TEXT NOT NULL, parameters TEXT, retrievedurl TEXT, pagetext TEXT);" []
-					x <- newChan
-					forkIO_ "kps:dblogchan" $ forever $ do
-						chanaction <- readChan x
-						chanaction opendb
-					return (Data.Map.insert filename x m, x)
-		writeChan chan action
-
-	sock <- KoL.HttpLowlevel.mklistensocket (listen_public _log_fakeref) portnum
-
-	forkIO_ "kps:updatedatafiles" $ HardcodedGameStuff.update_data_files
-	when (launch_browser_ $ environment_settings_ $ globalref) $ forkIO_ "kps:launchkolproxy" $ PlatformLowlevel.platform_launch portnum
-
-	let runLoop f = do
-		cont <- f
-		when cont $ runLoop f
-
-	debug_do "do_loop" $ runLoop $ handleConnection globalref (sessionmastermv, dblogstuff) sock (portnum, mvsequence, mvchat, logchan, dropping_logchan)
-
-	putStrLn "Shutting down."
 
 mkResponse code hdrs text = Response code "" (map (\(x,y) -> mkHeader (HdrCustom x) y) hdrs) text
 
@@ -506,3 +360,28 @@ kolProxyHandler uri params baseref = do
 				Left (pt, effuri) -> makeErrorResponse pt effuri []
 				Right (pt, effuri, ct) -> makeResponseWithNoExtraHeaders pt effuri [("Content-Type", ct), ("Cache-Control", "no-cache")]
 	return retresp
+
+setupHandlerChannels ref = do
+	let try_handling send_response x = do
+		let errhdrs = [("Content-Type", "text/plain; charset=UTF-8"), ("Cache-Control", "no-cache")]
+		result <- try x
+		(send_response $ case result of
+			Right resp -> resp
+			Left err -> Server.mkResponse (5,0,0) errhdrs $ Data.ByteString.Char8.pack $ "Error:\n\n" ++ show (err :: SomeException)) `catch` (\e -> do
+				putWarningStrLn $ "send_response exception: " ++ show (e :: SomeException))
+	mvsequence <- newChan
+	forkIO_ "kps:mvseq" $ forever $ do
+		(uri, params, cookie, send_response) <- readChan mvsequence
+		try_handling send_response $ debug_do ("sequence response for: " ++ show uri) $ do
+			Logging.log_file_retrieval ref ("proxy:" ++ show uri) params
+			Logging.log_time_uri ref "request" uri
+			resp <- Logging.log_time_interval ref ("creating response for: " ++ show uri) $ Server.kolProxyHandler uri params cookie
+			return resp
+	mvchat <- newChan
+	forkIO_ "kps:mvchat" $ forever $ do
+		(uri, params, cookie, send_response) <- readChan mvchat
+		forkIO_ "kps:mvchattry" $ do -- Serve asynced, possible private-message loss until server is fixed
+--              do -- Serve synced, possible hang on timeouts, still can't guarantee private-messages
+			try_handling send_response $ debug_do ("chat response for: " ++ show uri) $ Server.kolProxyHandlerChat uri params cookie
+	return (mvsequence, mvchat)
+
--- kolproxy.hs
+++ kolproxy.hs
@@ -1,17 +1,133 @@
 import Prelude
 import qualified Handlers
+import qualified HardcodedGameStuff
 import qualified Lua
 import qualified PlatformLowlevel
 import qualified Server
 import qualified KoL.Http
+import qualified KoL.HttpLowlevel
 import KoL.Util
 import KoL.UtilTypes
+import Control.Applicative
+import Control.Concurrent
 import Control.Exception
+import Control.Monad
+import Data.IORef
 import Data.Maybe
+import Data.Time
 import Network.URI
 import qualified System.Directory (doesFileExist, createDirectoryIfMissing)
 import qualified System.Environment (getArgs)
 import qualified System.IO
+import qualified System.Random
+import qualified Data.Map
+
+
+make_globalref = do
+	environment_settings <- do
+		let checkenv defaultvalue setting = do
+			value <- getEnvironmentSetting setting
+			case value of
+				Just "1" -> return True
+				Just "0" -> return False
+				_ -> return defaultvalue
+		listenpublic <- checkenv False "KOLPROXY_LISTEN_PUBLIC"
+		actionbarstate <- checkenv True "KOLPROXY_STORE_STATE_IN_ACTIONBAR"
+		localstate <- checkenv (not listenpublic) "KOLPROXY_STORE_STATE_LOCALLY"
+		launchbrowser <- checkenv (not listenpublic) "KOLPROXY_LAUNCH_BROWSER"
+		return $ EnvironmentSettings {
+			store_state_in_actionbar_ = actionbarstate,
+			store_state_locally_ = localstate,
+			store_ascension_logs_ = not listenpublic,
+			store_chat_logs_ = not listenpublic,
+			store_info_logs_ = not listenpublic,
+			listen_public_ = listenpublic,
+			launch_browser_ = launchbrowser
+		}
+	let openlog filename = do
+		h <- System.IO.openFile ("logs/info/" ++ filename) System.IO.AppendMode
+		System.IO.hSetBuffering h System.IO.LineBuffering
+		return h
+	indentref <- newIORef 0
+	blockluaref <- newIORef False
+	hfiles <- openlog "files-downloaded.txt"
+	htiming <- openlog "timing-log.txt"
+	hhttp <- openlog "http-log.txt"
+	shutdown_secret <- get_md5 <$> show <$> (System.Random.randomIO :: IO Integer)
+	chatopendb <- create_db "sqlite3 chatlog" "chat-log.sqlite3"
+	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS public(mid INTEGER PRIMARY KEY NOT NULL, time INTEGER NOT NULL, channel TEXT NOT NULL, playerid INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
+	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS private(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time INTEGER NOT NULL, playerid INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
+	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS other(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time INTEGER NOT NULL, msg TEXT NOT NULL, rawjson TEXT NOT NULL);" []
+	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS unrecognized(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, rawjson TEXT NOT NULL);" []
+	do_db_query_ chatopendb "CREATE TABLE IF NOT EXISTS oldchat(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text TEXT NOT NULL);" []
+	chatlogchan <- newChan
+	forkIO_ "kps:chatlogchan" $ forever $ do
+		chanaction <- readChan chatlogchan
+		chanaction chatopendb
+	use_slow_http_ref <- newIORef =<< do
+		envhttp10 <- getEnvironmentSetting "KOLPROXY_USE_HTTP10"
+		case envhttp10 of
+			Just "1" -> return True
+			Just "0" -> return False
+			_ -> KoL.HttpLowlevel.check_for_http10
+	tnow <- getCurrentTime
+	last_datafile_update_ref <- newIORef $ addUTCTime (fromInteger (-60000)) tnow
+	return GlobalRefStuff {
+		logindents_ = indentref,
+		blocking_lua_scripting_ = blockluaref,
+		h_files_downloaded_ = hfiles,
+		h_timing_log_ = htiming,
+		h_http_log_ = hhttp,
+		shutdown_secret_ = shutdown_secret,
+		doChatLogAction_ = \action -> writeChan chatlogchan action,
+		use_slow_http_ref_ = use_slow_http_ref,
+		lastDatafileUpdate_ = last_datafile_update_ref,
+		environment_settings_ = environment_settings
+	}
+
+kolproxy_setup_refstuff = do
+	(logchan, dropping_logchan) <- do
+		dropping_logchan <- newChan
+		forkIO_ "kps:droplogchan" $ forever $ readChan dropping_logchan
+		logchan <- newChan
+		forkIO_ "kps:logchan" $ forever $ debug_do "writelog" $ join $ readChan $ logchan
+		return (logchan, dropping_logchan)
+	globalref <- make_globalref
+	return (logchan, dropping_logchan, globalref)
+
+runProxyServer portnum = do
+	(logchan, dropping_logchan, globalref) <- kolproxy_setup_refstuff
+	-- TODO: get rid of fakeref here!
+	let _fake_other = OtherRefStuff { connection_ = undefined, sessionData_ = undefined }
+	let logref = LogRefStuff { logchan_ = logchan, solid_logchan_ = logchan }
+	let _log_fakeref = RefType { logstuff_ = logref, processPage_ = undefined, otherstuff_ = _fake_other, stateValid_ = undefined, globalstuff_ = globalref }
+
+	(mvsequence, mvchat) <- Server.setupHandlerChannels _log_fakeref
+
+	sessionmastermv <- newMVar Data.Map.empty
+	dblogmapmv <- newMVar Data.Map.empty
+	let dblogstuff filename action = do
+		chan <- modifyMVar dblogmapmv $ \m -> do
+			case Data.Map.lookup filename m of
+				Just x -> return (m, x)
+				Nothing -> do
+					putStrLn $ "DB: Opening log database: " ++ filename
+					opendb <- create_db "sqlite3 log" filename
+					do_db_query_ opendb "CREATE TABLE IF NOT EXISTS pageloads(idx INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time TEXT NOT NULL, statusbefore TEXT, statusafter TEXT, statebefore TEXT, stateafter TEXT, sessionid TEXT NOT NULL, requestedurl TEXT NOT NULL, parameters TEXT, retrievedurl TEXT, pagetext TEXT);" []
+					x <- newChan
+					forkIO_ "kps:dblogchan" $ forever $ do
+						chanaction <- readChan x
+						chanaction opendb
+					return (Data.Map.insert filename x m, x)
+		writeChan chan action
+	sock <- KoL.HttpLowlevel.mklistensocket (listen_public _log_fakeref) portnum
+	forkIO_ "kps:updatedatafiles" $ HardcodedGameStuff.update_data_files
+	when (launch_browser_ $ environment_settings_ $ globalref) $ forkIO_ "kps:launchkolproxy" $ PlatformLowlevel.platform_launch portnum
+	let runLoop f = do
+		cont <- f
+		when cont $ runLoop f
+	debug_do "do_loop" $ runLoop $ Server.handleConnection globalref (sessionmastermv, dblogstuff) sock (portnum, mvsequence, mvchat, logchan, dropping_logchan)
+	putStrLn "Shutting down."
 
 runKolproxy = (do
 	have_process_page <- System.Directory.doesFileExist "scripts/kolproxy-internal/process-page.lua"
@@ -36,7 +152,7 @@ runKolproxy = (do
 	let portnum = case portenv of
 		Just x -> fromJust $ read_as x :: Integer
 		Nothing -> 18481
-	Server.runProxyServer Server.kolProxyHandler Server.kolProxyHandlerChat portnum) `catch` (\e -> putDebugStrLn ("runKolproxy exception: " ++ show (e :: Control.Exception.SomeException)))
+	runProxyServer portnum) `catch` (\e -> putDebugStrLn ("runKolproxy exception: " ++ show (e :: Control.Exception.SomeException)))
 
 main = PlatformLowlevel.platform_init $ do
 	System.IO.hSetBuffering System.IO.stdout System.IO.LineBuffering
@@ -48,7 +164,7 @@ main = PlatformLowlevel.platform_init $ do
 	return ()
 
 runbot filename = do
-	(logchan, dropping_logchan, globalref) <- Server.kolproxy_setup_refstuff
+	(logchan, dropping_logchan, globalref) <- kolproxy_setup_refstuff
 
 	let login_useragent = kolproxy_version_string ++ " (" ++ PlatformLowlevel.platform_name ++ ")" ++ " BotScript/0.1 (" ++ filename ++ ")"
 	let login_host = fromJust $ parseURI $ "http://www.kingdomofloathing.com/"
